da4458a7405c8aa375fefc8bf77620bb98e2b18a
acin91 <z.aliakseyeu@gmail.com>
2014-09-13 18:37:43 +0300

    fix with mail content for text/html content-type

--- Message/Type/MailType.php
+++ Message/Type/MailType.php
@@ -51,14 +51,8 @@ class MailType implements MessageTypeInterface
      */
     public function build(array $configuration)
     {
-        // $message = \Swift_Message::newInstance($configuration['subject'])
-        //     ->setFrom($configuration['from'])
-        //     ->setTo($configuration['to'])
-        //     ->setBody($this->twig->render($configuration['body'], $configuration['body_data']))
-        // ;
-
-        // return $message;
-        $configuration['body'] = $this->twig->render($configuration['body'], $configuration['body_data']);
+        $content = $this->twig->render($configuration['body'], $configuration['body_data']);
+        $configuration['body'] = $this->twig->render('{{ "'.$content.'"|raw }}');
 
         return $configuration;
     }
